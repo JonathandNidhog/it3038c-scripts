684c1f2e5886d4cbf5a978e71cb2db26120eb323
JonathandNidhog <wdyv628@gmail.com>
2023-01-17 21:34:24 -0500

    Lab2

--- bash/covid2.sh
+++ bash/covid2.sh
@@ -3,7 +3,9 @@
 
 POSITIVE=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].positive')
 TODAY=$(date)
+NEGATIVE=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].negative')
+HOSP=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].hospitalized')
 ICU=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].inIcuCurrently')
 DEATH=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].death')
 DEATHIN=$(curl https://api.covidtracking.com/v1/us/current.json | jq '.[0].deathIncrease')
-echo "On $TODAY, there were $POSITIVE positive COVID cases,and $ICU people still in ICU, $DEATH already death because covid, the number of death increased $DEATHIN today."
+echo "On $TODAY, there were$POSITIVE positive,$NEGATIVE negative COVID cases,and$ICUpeople still in ICU,$DEATH already death because covid,and$HOSP still in hospital the number of death increased$DEATHIN today"
