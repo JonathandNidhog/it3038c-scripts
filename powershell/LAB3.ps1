39956f6c411d89db431feac6a6c4c78cda40fb09
Daiyu Wei <65467723+JonathandNidhog@users.noreply.github.com>
2023-01-30 18:57:51 -0500

    Lab3
    Lab3

---
+++ powershell/LAB3.ps1
@@ -0,0 +1,17 @@
+﻿$Hello = "Hello,powersell!"
+write-Host($Hello)
+function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "172*"
+}
+Write-Host(getIP)
+
+
+$IP = getIP
+$Date = Get-Date
+$Host1 = $Host.Version.Major
+$Hostname = Get-WmiObject Win32_ComputerSystem | select Name
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $Hostname. PowerShell version $Host1. Today's Date is $Date"
+
+Write-Host($Body)
+
+Send-MailMessage -To "weidu@mail.uc.edu" -From "wdyv628@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
